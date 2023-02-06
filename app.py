2c6d15e35dffc0307d0518c4bb2755914f1e1b45
jrodriguez5909 <jutiliano.rodriguez@gmail.com>
2023-02-06 23:11:01 +0100

    Tweaks to app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.write("""
 ## **App Instructions:**
 1. Populate the **`Required User Inputs`** section below
 2. Populate the **`Optional User Inputs`** section below if you'd like to only see results within a certain timeframe  
-3. Click the **`Show me all possible flights!`** button to kick-off the data pull. Note each iteration takes about 30 seconds to run so keep an eye on the status bar 
+3. Click the **`Show me all possible flights!`** button to kick-off the data pull. Note each iteration takes about **50 seconds** to run so keep an eye on the status bar 
 """)
 
 st.write("""
