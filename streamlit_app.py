6d38845b68db60d72c94ef81517e7cc90256f7ef
jrodriguez5909 <jutiliano.rodriguez@gmail.com>
2023-02-08 16:35:23 +0100

    Tweaks to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,16 @@ def scrape_permutations(urls):
             # driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),
             #                           chrome_options=chrome_options)
 
-            options = Options()
+            # options = Options()
+            # options.add_argument("--headless")
+            # options.add_argument("--no-sandbox")
+            # options.add_argument("--disable-dev-shm-usage")
+            # options.add_argument("--disable-gpu")
+            # options.add_argument("--disable-features=NetworkService")
+            # options.add_argument("--window-size=1920x1080")
+            # options.add_argument("--disable-features=VizDisplayCompositor")
+
+            options = webdriver.ChromeOptions()
             options.add_argument("--headless")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
